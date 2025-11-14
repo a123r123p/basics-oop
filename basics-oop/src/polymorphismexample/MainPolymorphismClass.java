e6caa93c1ceb66fa63e576c6ab1e743a10ccf825
asp-create <arpan107mondal@gmail.com>
2025-11-14 22:17:24 +0530

    removed new line

--- basics-oop/src/polymorphismexample/MainPolymorphismClass.java
+++ basics-oop/src/polymorphismexample/MainPolymorphismClass.java
@@ -5,7 +5,6 @@ public class MainPolymorphismClass {
         PerformSum performSum = new PerformSum();
         performSum.sum(2,3);
         performSum.sum(4,5,6);
-
         performSum.sum(3.45,8.90);
         performSum.sum(2.34f,1.23f);
         performSum.sum(3.45,2);
